3d9e3f3309fafb9376b72f5b879deefaa3097395
Adolfo Casari <acasari@casari-asoc.com>
2013-09-19 21:24:45 -0400

    My initial commit

---
+++ mydjsite/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'myworkspace.views.home', name='home'),
+    # url(r'^myworkspace/', include('myworkspace.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
